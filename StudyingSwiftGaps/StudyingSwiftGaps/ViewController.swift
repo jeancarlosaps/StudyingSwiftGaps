1089b620bfdda091d0f1092a82b6a988288eaacf
jean.santos <jean.santos@bancointer.com.br>
2020-12-15 21:13:08 -0300

    Starting project, configure cocoapos and Alamofire framework.

---
+++ StudyingSwiftGaps/StudyingSwiftGaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyingSwiftGaps
+//
+//  Created by Jean Carlos Antônio Pereira dos Santos on 15/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
